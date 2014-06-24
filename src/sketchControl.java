d97e137985a6bc4b532d8640d695e651d975fe32
sichenzhao <szhao@imac025.student.cs.uwaterloo.ca>
2014-06-23 20:24:12 -0400

    Another example
    http://examples.javacodegeeks.com/desktop-java/awt/event/a-simple-mouse-
    drag-example/

--- src/sketchControl.java
+++ src/sketchControl.java
@@ -1,3 +1,9 @@
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import javax.swing.JPanel;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -8,7 +14,35 @@
  * @author szhao
  */
 public class sketchControl extends JPanel
-                            implements MouseMotionListener{
+                            implements MouseListener, MouseMotionListener{
+
+    public void mouseDragged(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouseMoved(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouseClicked(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mousePressed(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouseReleased(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouseEntered(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouseExited(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
 
 }
